ae06772e1f5f09bfb53126af3840741c14980ef1
bjorneme <bjorn.melaaen@gmail.com>
2024-03-14 21:09:21 +0100

    feat: Implement Transformer model

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Todo
 
 1. Validation loss?
-2. Implement CNN
-3. Plot the models together
-4. Plot normalized
-5. Bar plot summarizing the different models
+2. Plot the models together
+3. Plot normalized
+4. Bar plot summarizing the different models
--- main.py
+++ main.py
@@ -2,21 +2,26 @@ from ForcastSystem import ForcastingSystem
 from models.LSTMModel import LSTMModel
 from models.CNNModel import CNNModel
 from models.GRUModel import GRUModel
+from models.TransformerModel import TransformerModel
 
 # The main function
 if __name__ == "__main__":
-    input_channels = 4  # As before, for month, day, hour, temperature
-    num_features = 24  # Number of temporal features after flattening
+    input_size = 4  # month, day, hour, temperature
+    num_heads = 2
+    num_encoder_layers = 2
+    num_decoder_layers = 2
+    dim_feedforward = 64
+    max_seq_length = 24
     output_size = 1
 
-    model = CNNModel(input_channels, num_features, output_size)
+    model = TransformerModel(input_size, num_heads, num_encoder_layers, num_decoder_layers, dim_feedforward, max_seq_length, output_size)
     
     system = ForcastingSystem(
         filepath="consumption_and_temperatures.csv",
         area_number=1,
         model = model,
-        num_epochs = 10,
+        num_epochs = 1,
         learning_rate = 0.001,
-        model_filepath = "models/pre_trained_models/cnn_model.pth"
+        model_filepath = "models/pre_trained_models/transformer_model.pth"
     )
     system.run_system()

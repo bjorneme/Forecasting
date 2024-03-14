4c79a4c8bd68ff937a96c96f233525f7d9d0c55b
bjorneme <bjorn.melaaen@gmail.com>
2024-03-14 20:53:51 +0100

    feat: Implement CNN Model

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from ForcastSystem import ForcastingSystem
 from models.LSTMModel import LSTMModel
 from models.CNNModel import CNNModel
-from models.GRUModel import GRUNet
+from models.GRUModel import GRUModel
 
 # The main function
 if __name__ == "__main__":
-    input_size = 4
-    hidden_layer_size = 24
+    input_channels = 4  # As before, for month, day, hour, temperature
+    num_features = 24  # Number of temporal features after flattening
     output_size = 1
-    num_layers = 2
 
-    model = GRUNet(input_size, hidden_layer_size, output_size, num_layers)
+    model = CNNModel(input_channels, num_features, output_size)
     
     system = ForcastingSystem(
         filepath="consumption_and_temperatures.csv",
@@ -18,6 +17,6 @@ if __name__ == "__main__":
         model = model,
         num_epochs = 10,
         learning_rate = 0.001,
-        model_filepath = "models/pre_trained_models/gru_model.pth"
+        model_filepath = "models/pre_trained_models/cnn_model.pth"
     )
     system.run_system()
